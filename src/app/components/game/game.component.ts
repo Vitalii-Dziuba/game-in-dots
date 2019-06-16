f934a0fc5039c3033e9a0ed50c309d50f0775ee5
Vitalik Dziuba <vetal.rvn@gmail.com>
2019-06-16 14:59:46 +0300

    bug-fix

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -131,7 +131,8 @@ export class GameComponent implements OnInit {
   }
 
   onSquareClick(i, j) {
-    if (this.currentSquare[0] === i && this.currentSquare[1] === j) {
+    if (this.currentSquare[0] === i && this.currentSquare[1] === j &&
+      this.table[this.currentSquare[0]][this.currentSquare[1]] === 1) {
       this.table[this.currentSquare[0]][this.currentSquare[1]] = 2;
       this.counter.green += 1;
     } else {
